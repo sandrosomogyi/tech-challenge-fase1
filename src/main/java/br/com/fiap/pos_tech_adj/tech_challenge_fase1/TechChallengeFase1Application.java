bd1435361e72ecbbf8e2614a6fd3b66446bc963b
sandrosomogyi <sdsandros86@hotmail.com>
2024-06-26 15:44:50 -0300

    Commit Inicial

---
+++ src/main/java/br/com/fiap/pos_tech_adj/tech_challenge_fase1/TechChallengeFase1Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.pos_tech_adj.tech_challenge_fase1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeFase1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengeFase1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/pos_tech_adj/tech_challenge_fase1/TechChallengeFase1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.pos_tech_adj.tech_challenge_fase1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeFase1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
